a5083f56ef482ffe73602d2dd4ff3f730a25add8
Pani Kumar <kumarpani@gmail.com>
2014-08-07 10:06:36 +0530

    Chapter3: Added Negative Tests

---
+++ RESTTests_RestSharp/Contract/ErrorResponseContainer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace RESTTests_RestSharp.Contract
+{
+    public class ErrorResponseContainer
+    {
+        public Error error { get; set; }
+    }
+    
+    public class ErrorsList
+    {
+        public string domain { get; set; }
+        public string code { get; set; }
+        public string internalReason { get; set; }
+    }
+
+    public class Error
+    {
+        public int code { get; set; }
+        public string message { get; set; }
+        public List<ErrorsList> errors { get; set; }
+    }
+}
---
+++ RESTTests_RestSharp/Tests/Negative/ParameterTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using RESTTests_RestSharp.Contract;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace RESTTests_RestSharp.Tests.Negative
+{
+    [TestFixture]
+    public class ParameterTests  
+    {
+        [Test]
+        public void ShouldReturn0ItemsWhenQParameterIsIncorrect()
+        {
+            var client = new RestClient("https://gdata.youtube.com/");
+
+            var request = new RestRequest("feeds/api/videos", Method.GET);
+            request.AddParameter("q", "1sAPUfRycSes");
+            request.AddParameter("v", "2");
+            request.AddParameter("alt", "jsonc");
+
+            var response = client.Execute<ResponseContainer>(request);
+
+            Assert.IsNull(response.Data.data.items, "Count did not match");
+            
+        }
+
+        [Test]
+        public void shouldReturnErrorWhenVersionRequestedIsIncorrect()
+        {
+            var client = new RestClient("https://gdata.youtube.com/");
+
+            var request = new RestRequest("feeds/api/videos", Method.GET);
+            request.AddParameter("v", "5");
+            request.AddParameter("alt", "jsonc");
+
+            var response = client.Execute<ErrorResponseContainer>(request);
+
+            Assert.AreEqual(403, response.Data.error.code, "Error code did not match");
+            Assert.AreEqual("Version 5 is not supported.", response.Data.error.message, "Message did not match");
+ 
+        }
+    }
+}
